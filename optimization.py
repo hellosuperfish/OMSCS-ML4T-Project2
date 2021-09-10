f3fdd478478ae019cb0e58a3339cd92a2b227227
Erin <emm0690@gmail.com>
2021-09-10 13:30:01 -0700

    Just before adding comments

---
+++ optimization.py
@@ -0,0 +1,168 @@
+""""""  		  	   		   	 		  		  		    	 		 		   		 		  
+"""MC1-P2: Optimize a portfolio.  		  	   		   	 		  		  		    	 		 		   		 		  
+  		  	   		   	 		  		  		    	 		 		   		 		  
+Copyright 2018, Georgia Institute of Technology (Georgia Tech)  		  	   		   	 		  		  		    	 		 		   		 		  
+Atlanta, Georgia 30332  		  	   		   	 		  		  		    	 		 		   		 		  
+All Rights Reserved  		  	   		   	 		  		  		    	 		 		   		 		  
+  		  	   		   	 		  		  		    	 		 		   		 		  
+Template code for CS 4646/7646  		  	   		   	 		  		  		    	 		 		   		 		  
+  		  	   		   	 		  		  		    	 		 		   		 		  
+Georgia Tech asserts copyright ownership of this template and all derivative  		  	   		   	 		  		  		    	 		 		   		 		  
+works, including solutions to the projects assigned in this course. Students  		  	   		   	 		  		  		    	 		 		   		 		  
+and other users of this template code are advised not to share it with others  		  	   		   	 		  		  		    	 		 		   		 		  
+or to make it available on publicly viewable websites including repositories  		  	   		   	 		  		  		    	 		 		   		 		  
+such as github and gitlab.  This copyright statement should not be removed  		  	   		   	 		  		  		    	 		 		   		 		  
+or edited.  		  	   		   	 		  		  		    	 		 		   		 		  
+  		  	   		   	 		  		  		    	 		 		   		 		  
+We do grant permission to share solutions privately with non-students such  		  	   		   	 		  		  		    	 		 		   		 		  
+as potential employers. However, sharing with other current or future  		  	   		   	 		  		  		    	 		 		   		 		  
+students of CS 7646 is prohibited and subject to being investigated as a  		  	   		   	 		  		  		    	 		 		   		 		  
+GT honor code violation.  		  	   		   	 		  		  		    	 		 		   		 		  
+  		  	   		   	 		  		  		    	 		 		   		 		  
+-----do not edit anything above this line---  		  	   		   	 		  		  		    	 		 		   		 		  
+  		  	   		   	 		  		  		    	 		 		   		 		  
+Student Name: Erin MIddlemas 		  	   		   	 		  		  		    	 		 		   		 		  
+GT User ID: emiddlemas6  		  	   		   	 		  		  		    	 		 		   		 		  
+GT ID: 903740356   		  	   		   	 		  		  		    	 		 		   		 		  
+"""  		  	   		   	 		  		  		    	 		 		   		 		  
+  		  	   		   	 		  		  		    	 		 		   		 		  
+  		  	   		   	 		  		  		    	 		 		   		 		  
+import datetime as dt  		  	   		   	 		  		  		    	 		 		   		 		  
+  		  	   		   	 		  		  		    	 		 		   		 		  
+import numpy as np  		  	   		   	 		  		  		    	 		 		   		 		  
+  		  	   		   	 		  		  		    	 		 		   		 		  
+import matplotlib.pyplot as plt  		  	   		   	 		  		  		    	 		 		   		 		  
+import pandas as pd  		  	   		   	 		  		  		    	 		 		   		 		  
+from util import get_data, plot_data
+import scipy.optimize as sco
+  		  	   		   	 		  		  		    	 		 		   		 		  
+  		  	   		   	 		  		  		    	 		 		   		 		  
+# This is the function that will be tested by the autograder  		  	   		   	 		  		  		    	 		 		   		 		  
+# The student must update this code to properly implement the functionality  		  	   		   	 		  		  		    	 		 		   		 		  
+def optimize_portfolio(  		  	   		   	 		  		  		    	 		 		   		 		  
+    sd=dt.datetime(2008, 1, 1),
+    ed=dt.datetime(2009, 1, 1),
+    syms=["GOOG", "AAPL", "GLD", "XOM"],  		  	   		   	 		  		  		    	 		 		   		 		  
+    gen_plot=False,  		  	   		   	 		  		  		    	 		 		   		 		  
+):  		  	   		   	 		  		  		    	 		 		   		 		  
+    """  		  	   		   	 		  		  		    	 		 		   		 		  
+    This function should find the optimal allocations for a given set of stocks. You should optimize for maximum Sharpe  		  	   		   	 		  		  		    	 		 		   		 		  
+    Ratio. The function should accept as input a list of symbols as well as start and end dates and return a list of  		  	   		   	 		  		  		    	 		 		   		 		  
+    floats (as a one-dimensional numpy array) that represents the allocations to each of the equities. You can take  		  	   		   	 		  		  		    	 		 		   		 		  
+    advantage of routines developed in the optional assess portfolio project to compute daily portfolio value and  		  	   		   	 		  		  		    	 		 		   		 		  
+    statistics.  		  	   		   	 		  		  		    	 		 		   		 		  
+  		  	   		   	 		  		  		    	 		 		   		 		  
+    :param sd: A datetime object that represents the start date, defaults to 1/1/2008  		  	   		   	 		  		  		    	 		 		   		 		  
+    :type sd: datetime  		  	   		   	 		  		  		    	 		 		   		 		  
+    :param ed: A datetime object that represents the end date, defaults to 1/1/2009  		  	   		   	 		  		  		    	 		 		   		 		  
+    :type ed: datetime  		  	   		   	 		  		  		    	 		 		   		 		  
+    :param syms: A list of symbols that make up the portfolio (note that your code should support any  		  	   		   	 		  		  		    	 		 		   		 		  
+        symbol in the data directory)  		  	   		   	 		  		  		    	 		 		   		 		  
+    :type syms: list  		  	   		   	 		  		  		    	 		 		   		 		  
+    :param gen_plot: If True, optionally create a plot named plot.png. The autograder will always call your  		  	   		   	 		  		  		    	 		 		   		 		  
+        code with gen_plot = False.  		  	   		   	 		  		  		    	 		 		   		 		  
+    :type gen_plot: bool  		  	   		   	 		  		  		    	 		 		   		 		  
+    :return: A tuple containing the portfolio allocations, cumulative return, average daily returns,  		  	   		   	 		  		  		    	 		 		   		 		  
+        standard deviation of daily returns, and Sharpe ratio  		  	   		   	 		  		  		    	 		 		   		 		  
+    :rtype: tuple  		  	   		   	 		  		  		    	 		 		   		 		  
+    """  		  	   		   	 		  		  		    	 		 		   		 		  
+  		  	   		   	 		  		  		    	 		 		   		 		  
+    # Read in adjusted closing prices for given symbols, date range  		  	   		   	 		  		  		    	 		 		   		 		  
+    dates = pd.date_range(sd, ed)  		  	   		   	 		  		  		    	 		 		   		 		  
+    prices_all = get_data(syms, dates)  # automatically adds SPY  		  	   		   	 		  		  		    	 		 		   		 		  
+    prices = prices_all[syms]  # only portfolio symbols  		  	   		   	 		  		  		    	 		 		   		 		  
+    prices_SPY = prices_all["SPY"]  # only SPY, for comparison later  		  	   		   	 		  		  		    	 		 		   		 		  
+
+   
+
+    #calculate daily returns
+    noa = len(syms)
+    allocs = np.array(noa*[1./noa,])
+    def daily_return_alloc(df,allocs):
+
+        prices_normed = df/df.iloc[0]
+        port_val_alloc = (prices_normed*allocs).sum(axis = 1)
+        daily_rets = port_val_alloc.copy()
+        daily_rets[1:] = (daily_rets[1:]/daily_rets[:-1].values)-1
+        daily_rets[0] = 0
+        return  daily_rets
+
+    #Define Sharpe Function
+    def min_func_sharpe(allocs, df):
+        daily_rets = daily_return_alloc(df,allocs)
+        SR = np.sqrt(252)*daily_rets.mean()/daily_rets.std()
+        return -SR
+
+
+
+
+    cons = ({'type': 'eq', 'fun':lambda x: np.sum(x) - 1})
+    bnds = tuple((0,1) for x in range(noa))
+    opts = sco.minimize(min_func_sharpe, allocs,method = 'SLSQP', bounds = bnds, constraints = cons, args = (prices))
+
+    # add code here to find the allocations
+    new_allocs = opts['x']
+
+
+
+
+    normed = prices/prices.iloc[0]
+    pos_vals = normed*new_allocs
+    port_val = pos_vals.sum(axis =1)
+    daily_rets_port = daily_return_alloc(prices,new_allocs)[1:]
+    cr = (port_val[-1]/port_val[0]-1)
+    adr = daily_rets_port.mean()
+    sddr = daily_rets_port.std()
+    sr = np.sqrt(252)* adr/sddr
+
+
+    # Get daily portfolio value  		  	   		   	 		  		  		    	 		 		   		 		  
+    #port_val = (prices/prices[0])*new_allocs  # add code here to compute daily portfolio values
+  		  	   		   	 		  		  		    	 		 		   		 		  
+    # Compare daily portfolio value with SPY using a normalized plot  		  	   		   	 		  		  		    	 		 		   		 		  
+    if gen_plot:  		  	   		   	 		  		  		    	 		 		   		 		  
+        # add code to plot here
+        prices_SPY = prices_SPY/prices_SPY.iloc[0]
+        df_temp = pd.concat(  		  	   		   	 		  		  		    	 		 		   		 		  
+            [port_val, prices_SPY], keys=["Portfolio", "SPY"], axis=1  		  	   		   	 		  		  		    	 		 		   		 		  
+        )
+        ax = df_temp.plot(title="Daily Portfolio vs. SPY", fontsize = 12)
+        ax.set_xlabel("Date")
+        ax.set_ylabel("Normalized Price")
+        plt.savefig("Figure1.png")
+
+
+  		  	   		   	 		  		  		    	 		 		   		 		  
+    return (new_allocs, cr, adr, sddr, sr)
+  		  	   		   	 		  		  		    	 		 		   		 		  
+  		  	   		   	 		  		  		    	 		 		   		 		  
+def test_code():  		  	   		   	 		  		  		    	 		 		   		 		  
+    """  		  	   		   	 		  		  		    	 		 		   		 		  
+    This function WILL NOT be called by the auto grader.  		  	   		   	 		  		  		    	 		 		   		 		  
+    """  		  	   		   	 		  		  		    	 		 		   		 		  
+  		  	   		   	 		  		  		    	 		 		   		 		  
+    start_date = dt.datetime(2008, 6, 1)
+    end_date = dt.datetime(2009, 6, 1)
+    symbols = ["IBM", "X", "GLD", "JPM"]
+
+
+    # Assess the portfolio
+    allocations, cr, adr, sddr, sr = optimize_portfolio(
+        sd=start_date, ed=end_date, syms=symbols, gen_plot=False
+    )
+
+    # Print statistics
+    print(f"Start Date: {start_date}")
+    print(f"End Date: {end_date}")
+    print(f"Symbols: {symbols}")
+    print(f"Allocations:{allocations}")
+    print(f"Sharpe Ratio: {sr}")  		  	   		   	 		  		  		    	 		 		   		 		  
+    print(f"Volatility (stdev of daily returns): {sddr}")  		  	   		   	 		  		  		    	 		 		   		 		  
+    print(f"Average Daily Return: {adr}")  		  	   		   	 		  		  		    	 		 		   		 		  
+    print(f"Cumulative Return: {cr}")  		  	   		   	 		  		  		    	 		 		   		 		  
+  		  	   		   	 		  		  		    	 		 		   		 		  
+  		  	   		   	 		  		  		    	 		 		   		 		  
+if __name__ == "__main__":  		  	   		   	 		  		  		    	 		 		   		 		  
+    # This code WILL NOT be called by the auto grader  		  	   		   	 		  		  		    	 		 		   		 		  
+    # Do not assume that it will be called  		  	   		   	 		  		  		    	 		 		   		 		  
+    test_code()  		  	   		   	 		  		  		    	 		 		   		 		  
